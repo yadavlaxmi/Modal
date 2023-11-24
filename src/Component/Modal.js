d79b0815c7436244f769f6df962ff8009044bb7b
navgurukul <laxmiyadav21@navgurukul.org>
2023-11-24 12:49:37 +0530

    normal modal

---
+++ src/Component/Modal.js
@@ -0,0 +1,26 @@
+import React,{useState} from "react";
+const Modal=()=>{
+    const[showModal,setShowModal]=useState(false);
+    
+    const MyModal=()=>{
+        return(
+        <>
+        <h2>STAY TUNED</h2>
+            <p> Subscribe to our newsletter and never miss our designs,latest
+            <br></br>news.etc.Our newsletter is sent once a week ,every Monday
+            </p>
+        <button> Accept it </button>
+        </>
+        )
+    }
+    return(
+        <>
+        <div>
+         <button onClick={()=>setShowModal(true)}>open Modal</button>
+         {showModal && <MyModal/>}
+        </div>
+        </>
+
+    )
+}
+export default Modal;
