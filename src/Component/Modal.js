e52540fb644bc7517c293e62afec2fc6c6cfaf78
navgurukul <laxmiyadav21@navgurukul.org>
2023-11-24 18:48:58 +0530

    give style for page

--- src/Component/Modal.js
+++ src/Component/Modal.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import MyModal from "./ShowModal";
 const Modal=()=>{
+  
     const[showModal,setShowModal]=useState(false);
     const closeModal  =()=>  setShowModal(false)
     
@@ -8,9 +9,77 @@ const Modal=()=>{
     return(
         <>
         <div>
-         <button onClick={()=>setShowModal(true)}>open Modal</button>
+         <button className="modal-btn"onClick={()=>setShowModal(true)}>
+            open Modal
+        </button>
          {showModal && <MyModal closeModal={closeModal}/>}
         </div>
+        <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
+    <p>
+     A computer is a machine that can store and process 
+     information. Most computers 
+     rely on a binary system, which uses two variables, 0 and 1,
+      to complete tasks such as storing data, calculating algorithms, and displaying information   
+    </p>
         </>
 
     )
--- src/Component/ShowModal.js
+++ src/Component/ShowModal.js
@@ -1,14 +1,22 @@
-const MyModal=({closeModal})=>{
-    return(
+// import { cleanup } from "@testing-library/react"
+import { useEffect } from "react"
 
+const MyModal=({closeModal})=>{
+    useEffect(()=>{
+        document.body.style.overflowY="hidden";
+        return()=>{
+            // cleanup;
+        };
+    },[])
+    return(
     <>
-    <div className="modal-wrapper"></div>
+    <div className="modal-wrapper" onClick={closeModal}></div>
     <div className="modal-container">
     <h2>STAY TUNED</h2>
         <p> Subscribe to our newsletter and never miss our designs,latest
         <br></br>news.etc.Our newsletter is sent once a week ,every Monday
         </p>
-    <button onClick={closeModal}> Accept it </button>
+    <button className="modal-btn" onClick={closeModal}> Accept it </button>
 </div>
     </>
     )
